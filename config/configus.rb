5f685d4a371860285d32a36987319478ceffc1b8
Anvar Tuykin <tuykin@gmail.com>
2013-06-12 07:44:34 +0400

    Добавил configus для аттрибутов моделей

--- config/configus.rb
+++ config/configus.rb
@@ -2,6 +2,9 @@
 Configus.build Rails.env do
   env :production do
     website_url 'http://example.com'
+
+    event_attributes [:event_id, :name, :start_date, :start_time, :end_date, :end_time, :venue, :description, :link, :area, :categories, :modified, :recurring, :link_url, :link_name, :ticket_link, :venue_id, :latitude, :longitude, :user_id]
+    area_attributes [:area_id, :name, :keyword, :parent, :timezone, :latitude, :longitude, :tzid]
   end
 
   env :development, parent: :production do
